49fa6d459df92e4bcd6ab55eb86d9332e2924576
LWY <668915+laxa88@users.noreply.github.com>
2020-05-18 22:06:14 +0900

    cleanup code to initial texture render

--- 0x02-pixel/main01.cpp
+++ 0x02-pixel/main01.cpp
@@ -1,17 +1,76 @@
 #include <SDL.h>
+#include <stdio.h>
+#include <random>
+#include <cstring>
 
-int main(int argc, char *argv[])
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+bool init();
+bool loadMedia();
+void close();
+
+SDL_Window *window = NULL;
+SDL_Renderer *renderer = NULL;
+SDL_Texture *texture = NULL;
+SDL_Event windowEvent;
+
+bool init()
 {
-    SDL_LogSetAllPriority(SDL_LOG_PRIORITY_WARN);
-
-    if (SDL_Init(SDL_INIT_VIDEO) == 0)
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
     {
-        SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
-        return 1;
+        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+        return false;
     }
 
-    SDL_Log("Worked fine!");
+    window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if (window == NULL)
+    {
+        printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+        return false;
+    }
+
+    return true;
+}
+
+void close()
+{
+    SDL_DestroyWindow(window);
+    window = NULL;
+
     SDL_Quit();
+}
+
+int main(int argc, char *args[])
+{
+    //Start up SDL and create window
+    if (!init())
+    {
+        printf("Failed to initialize!\n");
+        close();
+        return 0;
+    }
+
+    int posX = 0, posY = 0;
+    renderer = SDL_CreateRenderer(window, -1, 0);
+    texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STREAMING, 8, 8);
+
+    while (true)
+    {
+        if (SDL_PollEvent(&windowEvent))
+        {
+            if (SDL_QUIT == windowEvent.type)
+            {
+                break;
+            }
+        }
+
+        SDL_SetRenderDrawColor(renderer, rand() % 255, rand() % 255, rand() % 255, rand() % 255);
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
+    }
+
+    close();
 
     return 0;
 }
