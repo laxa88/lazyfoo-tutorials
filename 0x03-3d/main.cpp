4347a9473a692726515671cbb1579faf026701fb
LWY <668915+laxa88@users.noreply.github.com>
2020-05-19 23:08:03 +0900

    refactor code for emscripten build

--- 0x03-3d/main.cpp
+++ 0x03-3d/main.cpp
@@ -11,6 +11,9 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_opengl.h>
 #include <GL/gl.h>
+#ifdef __EMSCRIPTEN__
+#include <emscripten.h>
+#endif
 
 typedef int32_t i32;
 typedef uint32_t u32;
@@ -19,54 +22,76 @@ typedef int32_t b32;
 #define WinWidth 1000
 #define WinHeight 1000
 
-int main(int ArgCount, char **Args)
-{
+u32 WindowFlags = SDL_WINDOW_OPENGL;
+SDL_Window *Window;
 
-    u32 WindowFlags = SDL_WINDOW_OPENGL;
-    SDL_Window *Window = SDL_CreateWindow("OpenGL Test", 0, 0, WinWidth, WinHeight, WindowFlags);
+b32 Running = 1;
+b32 FullScreen = 0;
+
+void init()
+{
+    Window = SDL_CreateWindow("OpenGL Test", 0, 0, WinWidth, WinHeight, WindowFlags);
     assert(Window);
     SDL_GLContext Context = SDL_GL_CreateContext(Window);
+}
 
-    b32 Running = 1;
-    b32 FullScreen = 0;
-    while (Running)
+void game_loop()
+{
+    SDL_Event Event;
+    while (SDL_PollEvent(&Event))
     {
-        SDL_Event Event;
-        while (SDL_PollEvent(&Event))
+        if (Event.type == SDL_KEYDOWN)
         {
-            if (Event.type == SDL_KEYDOWN)
+            switch (Event.key.keysym.sym)
             {
-                switch (Event.key.keysym.sym)
-                {
-                case SDLK_ESCAPE:
-                    Running = 0;
-                    break;
-                case 'f':
-                    FullScreen = !FullScreen;
-                    if (FullScreen)
-                    {
-                        SDL_SetWindowFullscreen(Window, WindowFlags | SDL_WINDOW_FULLSCREEN_DESKTOP);
-                    }
-                    else
-                    {
-                        SDL_SetWindowFullscreen(Window, WindowFlags);
-                    }
-                    break;
-                default:
-                    break;
-                }
-            }
-            else if (Event.type == SDL_QUIT)
-            {
+            case SDLK_ESCAPE:
                 Running = 0;
+                break;
+            case 'f':
+                FullScreen = !FullScreen;
+                if (FullScreen)
+                {
+                    SDL_SetWindowFullscreen(Window, WindowFlags | SDL_WINDOW_FULLSCREEN_DESKTOP);
+                }
+                else
+                {
+                    SDL_SetWindowFullscreen(Window, WindowFlags);
+                }
+                break;
+            default:
+                break;
             }
         }
-
-        glViewport(0, 0, WinWidth, WinHeight);
-        glClearColor(1.f, 0.f, 1.f, 0.f);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        SDL_GL_SwapWindow(Window);
+        else if (Event.type == SDL_QUIT)
+        {
+            Running = 0;
+        }
     }
+
+    glViewport(0, 0, WinWidth, WinHeight);
+    glClearColor(1.f, 0.f, 1.f, 0.f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    SDL_GL_SwapWindow(Window);
+}
+
+void main_loop()
+{
+#ifdef __EMSCRIPTEN__
+    emscripten_set_main_loop(game_loop, 0, 1);
+#else
+    while (Running)
+    {
+        game_loop();
+    }
+#endif
+}
+
+int main(int ArgCount, char **Args)
+{
+    init();
+
+    main_loop();
+
     return 0;
 }
