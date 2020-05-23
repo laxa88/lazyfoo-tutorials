87495cb9416e3c4a136a49c0c8efc91d263c5712
LWY <668915+laxa88@users.noreply.github.com>
2020-05-23 22:06:58 +0900

    add 0x05 project

---
+++ 0x05-2d-manip/main.cpp
@@ -0,0 +1,158 @@
+// references:
+// https://lazyfoo.net/tutorials/SDL/04_key_presses/index.php
+// https://lazyfoo.net/tutorials/SDL/05_optimized_surface_loading_and_soft_stretching/index.php
+// https://lazyfoo.net/tutorials/SDL/07_texture_loading_and_rendering/index.php
+
+#include <SDL2/SDL.h>
+#include <cstring>
+
+#include "wyngine/game.h"
+#include "wyngine/keyboard.h"
+#include "wyngine/image.h"
+#include "wyngine/random.h"
+
+class Game : public Wyngine
+{
+private:
+    SDL_Event windowEvent;
+
+    SDL_Texture *gKeyPressTextures[KEY_PRESS_SURFACE_TOTAL];
+    SDL_Texture *gCurrentTexture = NULL;
+
+    // TODO
+    // replace PNG load with PNG load
+    // check desktop build and size
+    // check JS output and build size
+    // refactor to use spritesheet instead
+    // add animation logic for spritesheet
+    // publish new post
+
+    bool loadMedia()
+    {
+        //Loading success flag
+        bool success = true;
+
+        //Load default surface
+        gKeyPressTextures[KEY_PRESS_SURFACE_DEFAULT] = LoadBMP(renderer, "assets/press.bmp");
+        if (gKeyPressTextures[KEY_PRESS_SURFACE_DEFAULT] == NULL)
+        {
+            printf("Failed to load default image!\n");
+            success = false;
+        }
+
+        //Load up surface
+        gKeyPressTextures[KEY_PRESS_SURFACE_UP] = LoadBMP(renderer, "assets/up.bmp");
+        if (gKeyPressTextures[KEY_PRESS_SURFACE_UP] == NULL)
+        {
+            printf("Failed to load up image!\n");
+            success = false;
+        }
+
+        //Load down surface
+        gKeyPressTextures[KEY_PRESS_SURFACE_DOWN] = LoadBMP(renderer, "assets/down.bmp");
+        if (gKeyPressTextures[KEY_PRESS_SURFACE_DOWN] == NULL)
+        {
+            printf("Failed to load down image!\n");
+            success = false;
+        }
+
+        //Load left surface
+        gKeyPressTextures[KEY_PRESS_SURFACE_LEFT] = LoadBMP(renderer, "assets/left.bmp");
+        if (gKeyPressTextures[KEY_PRESS_SURFACE_LEFT] == NULL)
+        {
+            printf("Failed to load left image!\n");
+            success = false;
+        }
+
+        //Load right surface
+        gKeyPressTextures[KEY_PRESS_SURFACE_RIGHT] = LoadBMP(renderer, "assets/right.bmp");
+        if (gKeyPressTextures[KEY_PRESS_SURFACE_RIGHT] == NULL)
+        {
+            printf("Failed to load right image!\n");
+            success = false;
+        }
+
+        return success;
+    }
+
+public:
+    void foo()
+    {
+    }
+
+    Game() : Wyngine("Wyngine", 1024, 768, 2)
+    {
+        if (gameRunning)
+        {
+            loadMedia();
+
+            gCurrentTexture = gKeyPressTextures[KEY_PRESS_SURFACE_DEFAULT];
+        }
+    }
+
+    void onUpdate()
+    {
+        if (SDL_PollEvent(&windowEvent))
+        {
+            if (SDL_QUIT == windowEvent.type)
+            {
+                gameRunning = false;
+            }
+            else if (windowEvent.type == SDL_KEYDOWN)
+            {
+                switch (windowEvent.key.keysym.sym)
+                {
+                case SDLK_UP:
+                    gCurrentTexture = gKeyPressTextures[KEY_PRESS_SURFACE_UP];
+                    break;
+
+                case SDLK_DOWN:
+                    gCurrentTexture = gKeyPressTextures[KEY_PRESS_SURFACE_DOWN];
+                    break;
+
+                case SDLK_LEFT:
+                    gCurrentTexture = gKeyPressTextures[KEY_PRESS_SURFACE_LEFT];
+                    break;
+
+                case SDLK_RIGHT:
+                    gCurrentTexture = gKeyPressTextures[KEY_PRESS_SURFACE_RIGHT];
+                    break;
+
+                default:
+                    gCurrentTexture = gKeyPressTextures[KEY_PRESS_SURFACE_DEFAULT];
+                    break;
+                }
+            }
+        }
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+        SDL_RenderClear(renderer);
+
+        // Initialize texture pixels to a red opaque RGBA value
+        // unsigned char *bytes = nullptr;
+        // int pitch = 0; // row of pixels
+        // SDL_LockTexture(texture, NULL, reinterpret_cast<void **>(&bytes), &pitch);
+        // for (int y = 0; y < texH; y++)
+        // {
+        //     for (int x = 0; x < texW; x++)
+        //     {
+        //         unsigned char rgba[4] = {random(255), random(255), random(255), random(255)};
+        //         memcpy(&bytes[(y * texW + x) * sizeof(rgba)], rgba, sizeof(rgba));
+        //     }
+        // }
+        // SDL_UnlockTexture(texture);
+
+        // SDL_RenderCopy(renderer, texture, NULL, NULL);
+        SDL_RenderCopy(renderer, gCurrentTexture, NULL, NULL);
+        SDL_RenderPresent(renderer);
+    }
+};
+
+int main(int argc, char *args[])
+{
+    Game *game = new Game();
+
+    game->run();
+
+    return 0;
+}
---
+++ 0x05-2d-manip/wyngine/game.h
@@ -0,0 +1,103 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <stdio.h>
+
+#ifdef __EMSCRIPTEN__
+#include <emscripten.h>
+#endif
+
+void emscriptenOnUpdate(void *arg);
+
+class Wyngine
+{
+protected:
+    const char *windowTitle;
+    int screenW;
+    int screenH;
+    int pixelSize;
+    int texW;
+    int texH;
+    bool gameRunning;
+
+    SDL_Window *window = NULL;
+    SDL_Renderer *renderer = NULL;
+    SDL_Texture *texture = NULL;
+
+public:
+    Wyngine() : Wyngine("Wyngine", 640, 480, 2)
+    {
+    }
+
+    Wyngine(const char *title, int w, int h, int ps)
+    {
+        windowTitle = title;
+        screenW = w;
+        screenH = h;
+        pixelSize = ps;
+        texW = w / ps;
+        texH = h / ps;
+
+        if (init())
+        {
+            gameRunning = true;
+        }
+    }
+
+    ~Wyngine()
+    {
+        SDL_DestroyWindow(window);
+        window = NULL;
+
+        SDL_Quit();
+    }
+
+    virtual void onUpdate() {}
+
+    void run()
+    {
+#ifdef __EMSCRIPTEN__
+        emscripten_set_main_loop_arg(emscriptenOnUpdate, this, 0, 1);
+#else
+        while (gameRunning)
+        {
+            onUpdate();
+        }
+#endif
+    }
+
+private:
+    bool init()
+    {
+        if (SDL_Init(SDL_INIT_VIDEO) < 0)
+        {
+            printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+            return false;
+        }
+
+        int imgFlags = IMG_INIT_PNG;
+        if (!(IMG_Init(imgFlags) & imgFlags))
+        {
+            printf("SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError());
+            return false;
+        }
+
+        window = SDL_CreateWindow(windowTitle, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, screenW, screenH, SDL_WINDOW_SHOWN);
+        if (window == NULL)
+        {
+            printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+            return false;
+        }
+
+        renderer = SDL_CreateRenderer(window, -1, 0);
+        texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STREAMING, texW, texH);
+
+        return true;
+    }
+};
+
+// Reference: https://stackoverflow.com/questions/55415179/unable-to-pass-a-proper-lambda-to-emscripten-set-main-loop
+// Emscripten only supports plain functions, not member functions
+void emscriptenOnUpdate(void *arg)
+{
+    static_cast<Wyngine *>(arg)->onUpdate();
+}
